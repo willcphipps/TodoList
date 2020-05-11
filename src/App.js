026696e3f5a82aaef703b69879d244ea5d7de9db
William Phipps <willcphipps@gmail.com>
2020-05-11 18:35:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [message, setMessage] = useState("");
+  
+  const handleMessage = (e) => {
+    e.preventDefault();
+    console.log(todo);
+    let item = { message, isComplete : false } ;
+    setTodo(prevTodo => [item, ...prevTodo]);
+    setMessage("");
+  }
+  const handleTodo = (e) => {
+    setMessage(e.target.value)
+  }
+  const onComplete = (e, id) => {
+    let thisTodo = [...todo];
+    thisTodo[id].isComplete = true;
+    setTodo(thisTodo);
+  }
+  const unComplete = (e, id) => {
+    let thisTodo = [...todo];
+    thisTodo[id].isComplete = false;
+    setTodo(thisTodo);
+  }
+  const destroy = (e, id) => {
+    let thisTodo = [...todo];
+    thisTodo.splice(id, 1);
+    setTodo(thisTodo);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="m-3 flex-column">
+        <form onSubmit={(e) => handleMessage(e)}>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => handleTodo(e)}
+          >
+          </input>
+        </form>
+        <div className="flex-column justify-content-center">
+          {todo.map((item, i) =>  
+            <div
+              className="text-left"
+              key={i}
+            >
+              <button
+                onClick={(e) => destroy(e, i)}
+                className="m-2 btn btn-sm btn-outline-danger">&#x2717;
+              </button>
+            {
+              item.isComplete ?
+                <button
+                  onClick={(e) => unComplete(e, i)}
+                  className="m-2 btn btn-sm btn-outline-warning">&#x2605;
+                </button>
+                :
+                <button
+                  onClick={(e) => onComplete(e, i)}
+                  className="m-2 btn btn-sm btn-outline-primary">&#9711;</button>
+              }
+              <span style={{
+                borderBottom: "1px solid gray",
+                borderRadius: "25px",
+                }} className="ml-3 p-2">{item.message}</span>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
