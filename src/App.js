eabe338dac81f89583ff9bd3f6abcc77fee7f713
William Phipps <willcphipps@gmail.com>
2020-05-12 12:33:24 -0500

    adding a touch of style

--- src/App.js
+++ src/App.js
@@ -34,45 +34,85 @@ function App() {
   }
   return (
     <div className="App">
-      <div className="m-3 flex-column">
+      <div className="jumbotron jumbotron-fluid m-5">
+        <h1 className="display-5">Todo App!</h1>
+        <p>Enter your items to do here</p>
+        <p className="lead">
         <form onSubmit={(e) => handleMessage(e)}>
           <input
             type="text"
-            value={message}
+            value={message} 
             onChange={(e) => handleTodo(e)}
           >
           </input>
         </form>
-        <div className="flex-column justify-content-center">
-          {todo.map((item, i) =>  
-            <div
-              className="text-left"
-              key={i}
-            >
-              <button
-                onClick={(e) => destroy(e, i)}
-                className="m-2 btn btn-sm btn-outline-danger">&#x2717;
-              </button>
-            {
-              item.isComplete ?
+        </p>
+        <p>Total things done: {" "} 
+          {
+            
+          todo.reduce((total, currentValue) => {
+            if (currentValue.isComplete) {
+              return total + 1
+            }
+            else {
+            return total;
+            }
+          },0
+            )}
+               </p>
+      </div>
+      <div className="m-3 flex-column">
+        {/* <form onSubmit={(e) => handleMessage(e)}>
+          <input
+            type="text"
+            value={message} 
+            onChange={(e) => handleTodo(e)}
+          >
+          </input>
+        </form> */}
+      </div>
+      <div className="d-flex flex-row justify-content-center">
+        <div className="flex-column">
+            {todo.map((item, i) =>  
+              <div
+                className="text-left"
+                key={i}
+              >
                 <button
-                  onClick={(e) => unComplete(e, i)}
-                  className="m-2 btn btn-sm btn-outline-warning">&#x2605;
+                  onClick={(e) => destroy(e, i)}
+                  className="ml-4 m-2 btn btn-sm btn-outline-danger">&#x2717;
                 </button>
-                :
-                <button
-                  onClick={(e) => onComplete(e, i)}
-                  className="m-2 btn btn-sm btn-outline-primary">&#9711;</button>
-              }
-              <span style={{
-                borderBottom: "1px solid gray",
-                borderRadius: "25px",
-                }} className="ml-3 p-2">{item.message}</span>
-            </div>
-          )}
+              {
+                item.isComplete ?
+                  <button
+                    onClick={(e) => unComplete(e, i)}
+                    className="text-left m-2 btn btn-sm btn-outline-warning">&#x2605;
+                  </button>
+                  :
+                  <button
+                    onClick={(e) => onComplete(e, i)}
+                    className="text-left m-2 btn btn-sm btn-outline-primary">&#9711;</button>
+                }
+                {
+                  item.isComplete ?
+                    <span style={{
+                      borderBottom: "1px solid gray",
+                      borderRadius: "25px",
+                      opacity: ".5"
+                    }} className="ml-3 p-2">{item.message}</span>
+                    : 
+                  <span style={{
+                    borderBottom: "1px solid gray",
+                    
+                    borderRadius: "25px",
+                  }} className="ml-3 p-2">{item.message}</span> 
+
+                }
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
